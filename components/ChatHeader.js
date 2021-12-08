8dbca699e91e3827de3b445c015c362a8586f5ce
amiteshkundan <amitesh.kundan@mckinleyrice.co>
2021-12-08 19:27:08 +0530

    contact sync expo install expo-image-picker npm i react-native-get-random-values

--- components/ChatHeader.js
+++ components/ChatHeader.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import {View,Text} from 'react-native'
+import { useRoute } from "@react-navigation/native";
+import GlobalContext from "../context/Context";
+import Avatar from "./Avatar";
 
 const  ChatHeader = ({size,user}) => {
     
-    return(
-            <View>
-                <Text> Header</Text>
-            </View>
+    const route = useRoute();
+    const {
+      theme: { colors },
+    } = useContext(GlobalContext);
+    return (
+      <View style={{ flexDirection: "row" }}>
+        <View>
+          <Avatar size={40} user={route.params.user} />
+        </View>
+        <View
+          style={{
+            marginLeft: 15,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: colors.white, fontSize: 18 }}>
+            {route.params.user.contactName || route.params.user.displayName}
+          </Text>
+        </View>
+      </View>
     );
 }
 export default ChatHeader
